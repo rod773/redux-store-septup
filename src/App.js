057b633ea5bc45e8f1bc13e146e080898fefcb95
rod773 <104315743+rod773@users.noreply.github.com>
2024-02-04 10:56:54 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,59 @@
-import "./App.css";
+import "./App.scss";
+import { legacy_createStore } from "redux";
+import { Provider, connect } from "react-redux";
+
+const countReducer = function (state = 0, action) {
+  switch (action.type) {
+    case "ADD":
+      return state + 1;
+    case "SUBSTRACT":
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+const store = legacy_createStore(countReducer);
+
+const mapStateToProps = (state) => {
+  return {
+    count: state,
+  };
+};
+
+const mapDispachToProps = (dispach) => {
+  return {
+    add: () => dispach({ type: "ADD" }),
+    substract: () => dispach({ type: "SUBSTRACT" }),
+  };
+};
+
+const Component = ({ count, add, substract }) => {
+  return (
+    <div>
+      <h1>Count = {count}</h1>
+      <div className="d-flex justify-content-between">
+        <button onClick={add} className="btn btn-primary">
+          ADD
+        </button>
+
+        <button onClick={substract} className="btn btn-success">
+          SUBSTRACT
+        </button>
+      </div>
+    </div>
+  );
+};
+
+const Container = connect(mapStateToProps, mapDispachToProps)(Component);
 
 function App() {
   return (
-    <div>
-      <h1>Redux Store Setup</h1>
+    <div className="container-fluid vh-100 d-flex flex-column align-items-center justify-content-center">
+      <Provider store={store}>
+        <h1>Redux Store Setup</h1>
+        <Container />
+      </Provider>
     </div>
   );
 }
